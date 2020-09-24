27f4ab5d6c270e2ee4ef5208680f9ac693c40bb9
Thiago Araujo <thd.araujo@gmail.com>
2020-09-24 12:32:42 -0700

    url validation

--- lib/patch_ruby/models/photo.rb
+++ lib/patch_ruby/models/photo.rb
@@ -65,12 +65,17 @@ module Patch
     # @return Array for valid properties with the reasons
     def list_invalid_properties
       invalid_properties = Array.new
+      if @url.nil?
+        invalid_properties.push('invalid value for "url", url cannot be nil.')
+      end
+
       invalid_properties
     end
 
     # Check to see if the all the properties in the model are valid
     # @return true if the model is valid
     def valid?
+      return false if @url.nil?
       true
     end
 
