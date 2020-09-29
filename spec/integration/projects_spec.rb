a151bed71e33a0be579fd48268d1e5526078388d
Thiago Araujo <thd.araujo@gmail.com>
2020-09-28 18:14:56 -0700

    rename to SANDBOX_API_KEY env variable

--- spec/integration/projects_spec.rb
+++ spec/integration/projects_spec.rb
@@ -1,6 +1,6 @@
 RSpec.describe 'Projects Integration' do
   Patch.configure do |config|
-    config.access_token = ENV['PATCH_RUBY_API_KEY']
+    config.access_token = ENV['SANDBOX_API_KEY']
   end
 
   it 'supports retrieve and list' do
