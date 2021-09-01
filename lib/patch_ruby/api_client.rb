15bfa8d55e54e80639cf6fba5301212b68915587
Paul Cothenet <pcothenet@gmail.com>
2021-08-31 17:47:07 -0700

    Bump version

--- lib/patch_ruby/api_client.rb
+++ lib/patch_ruby/api_client.rb
@@ -31,7 +31,7 @@ module Patch
     # @option config [Configuration] Configuration for initializing the object, default to Configuration.default
     def initialize(config = Configuration.default)
       @config = config
-      @user_agent = "patch-ruby/1.10.1"
+      @user_agent = "patch-ruby/1.10.2"
       @default_headers = {
         'Content-Type' => 'application/json',
         'User-Agent' => @user_agent
