74a901bd6beff88069735c2f76a9bdbf73bcd1cf
Vicky Enalen <enalenv@gmail.com>
2023-03-24 12:25:14 -0700

    API version 2.1.0 See https://docs.patch.io/#/changelog for changes

--- spec/integration/projects_spec.rb
+++ spec/integration/projects_spec.rb
@@ -65,6 +65,8 @@ RSpec.describe 'Projects Integration' do
     expect(inventory).to be_an_instance_of(Array)
     expect(inventory[0]).to be_an_instance_of(Patch::Inventory)
     expect(inventory[0].vintage_year).to be_an_instance_of(Integer)
+    expect(inventory[0].vintage_start_year).to be_an_instance_of(Integer)
+    expect(inventory[0].vintage_end_year).to be_an_instance_of(Integer)
     expect(inventory[0].amount_available).to be_an_instance_of(Integer)
     expect(inventory[0].price).to be_an_instance_of(Integer)
     expect(inventory[0].currency).to be_an_instance_of(String)
